b2383eab7e97ff48d26c5137a0266af5dec51e7c
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-04-17 11:26:33 -0400

    Made preproccesing work for handwash labeling and videos.
    In preproccesing I cut videos 3 sec before wrist appear(first event)
    and 3 sec after wrist disappear(last event)
    as well as resize videos to be 160x160.
    
    Added folder of generated preprocessed videos to gitignore.

--- data/preprocess_videos.py
+++ data/preprocess_videos.py
@@ -4,59 +4,69 @@ import cv2
 from multiprocessing import Pool
 import numpy as np
 
-df = pd.read_pickle('golfDB.pkl')
-yt_video_dir = '../../database/videos/'
+
+df = pd.read_pickle('handwash.pkl')
+
+video_dir = 'handwashing_videos/'
+destination_path = 'handwash_videos_{}/'.format(160)
 
 
-def preprocess_videos(anno_id, dim=160):
+def preprocess_videos(video_name, dim=160):
     """
-    Extracts relevant frames from youtube videos
+    Extracts relevant frames from videos
     """
 
-    a = df.loc[df['id'] == anno_id]
-    bbox = a['bbox'][0]
-    events = a['events'][0]
+    a = df.loc[df['video_name'] == video_name]
+    events = a['events'].item() #if confident that a single row will be returned
+    # events = a['events'].values[0] #select first row in an array of returned objects 
+    # with one corresponding to a single row
 
-    path = 'videos_{}/'.format(dim)
 
-    if not os.path.isfile(os.path.join(path, "{}.mp4".format(anno_id))):
-        print('Processing annotation id {}'.format(anno_id))
-        cap = cv2.VideoCapture(os.path.join(yt_video_dir, '{}.mp4'.format(a['youtube_id'][0])))
+
+    if not os.path.isfile(os.path.join(destination_path, "{}.mp4".format(video_name))):
+        print('Processing video with name {}.mp4'.format(video_name))
+        video_path = os.path.join(video_dir, '{}.mp4'.format(video_name))
+        cap = cv2.VideoCapture(video_path)
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        out = cv2.VideoWriter(os.path.join(path, "{}.mp4".format(anno_id)),
+        out = cv2.VideoWriter(os.path.join(destination_path, "{}.mp4".format(video_name)),
                               fourcc, cap.get(cv2.CAP_PROP_FPS), (dim, dim))
-        x = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) * bbox[0])
+        """x = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) * bbox[0])
         y = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * bbox[1])
         w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) * bbox[2])
-        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * bbox[3])
+        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * bbox[3])"""
         count = 0
-        success, image = cap.read()
+        success, frame = cap.read()
+
+        # print(success)
+        """frame = frame[:, :, [2, 1, 0]]
+        image = Image.fromarray(frame)"""
         while success:
             count += 1
-            if count >= events[0] and count <= events[-1]:
-                    crop_img = image[y:y + h, x:x + w]
+            # using frames from 3 sec before hands appear and 3 sec after they disappear.
+            if count >= events[0]-90 and count <= events[-1]+90: 
+                    """crop_img = image[y:y + h, x:x + w]
                     crop_size = crop_img.shape[:2]
                     ratio = dim / max(crop_size)
                     new_size = tuple([int(x*ratio) for x in crop_size])
-                    resized = cv2.resize(crop_img, (new_size[1], new_size[0]))
-                    delta_w = dim - new_size[1]
+                    resized = cv2.resize(crop_img, (new_size[1], new_size[0]))"""
+                    resized = cv2.resize(frame, (dim, dim))
+                    """delta_w = dim - new_size[1]
                     delta_h = dim - new_size[0]
                     top, bottom = delta_h // 2, delta_h - (delta_h // 2)
                     left, right = delta_w // 2, delta_w - (delta_w // 2)
                     b_img = cv2.copyMakeBorder(resized, top, bottom, left, right, cv2.BORDER_CONSTANT,
-                                               value=[0.406*255, 0.456*255, 0.485*255])  # ImageNet means (BGR)
-                    out.write(b_img)
-            if count > events[-1]:
+                                               value=[0.406*255, 0.456*255, 0.485*255]) """ # ImageNet means (BGR) as a color of border
+                    out.write(resized)
+            if count > events[-1]+90:
                 break
-            success, image = cap.read()
+            success, frame = cap.read()
     else:
-        print('Annotation id {} already completed for size {}'.format(anno_id, dim))
+        print('Video with name {} already completed for size {}'.format(video_name, dim))
 
 
 if __name__ == '__main__':
-    path = 'videos_{}/'.format(160)
-    if not os.path.exists(path):
-        os.mkdir(path)
-    preprocess_videos(df.id[1])
-    # p = Pool(6)
-    # p.map(preprocess_videos, df.id)
+    if not os.path.exists(destination_path):
+        os.mkdir(destination_path)
+    #preprocess_videos(df.video_name[0]) # to preprocess a single video
+    p = Pool(6) #multiprocessesing
+    p.map(preprocess_videos, df.video_name) # to preprocess all videos
