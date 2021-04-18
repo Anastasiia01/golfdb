3c64cfdf27ff4a28c8707e0836829120c511b259
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-04-18 11:48:09 -0400

    Did folder & model name changes  for eval to run.

--- eval.py
+++ eval.py
@@ -1,20 +1,20 @@
-from model import EventDetector
+from model import Handy
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from dataloader import GolfDB, ToTensor, Normalize
+from dataloader import HandwashDB, ToTensor, Normalize
 import torch.nn.functional as F
 import numpy as np
 from util import correct_preds
 
 
-def eval(model, split, seq_length, n_cpu, disp):
-    dataset = GolfDB(data_file='data/val_split_{}.pkl'.format(split),
-                     vid_dir='data/videos_160/',
-                     seq_length=seq_length,
-                     transform=transforms.Compose([ToTensor(),
-                                                   Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
-                     train=False)
+def eval(model, split, seq_length, n_cpu, device, disp):
+    dataset = HandwashDB(data_file='data/val_split_{}.pkl'.format(split),
+                        vid_dir='data/handwash_videos_160/',
+                        seq_length=seq_length,
+                        transform=transforms.Compose([ToTensor(),
+                                                    Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+                        train=False)
 
     data_loader = DataLoader(dataset,
                              batch_size=1,
@@ -28,7 +28,8 @@ def eval(model, split, seq_length, n_cpu, disp):
         images, labels = sample['images'], sample['labels']
         # full samples do not fit into GPU memory so evaluate sample in 'seq_length' batches
         batch = 0
-        while batch * seq_length < images.shape[1]:
+        # images.shape is [1, frames_count_per_video, 3, 160, 160]
+        while batch * seq_length < images.shape[1]: # images.shape[1] is frames_count_per_video
             if (batch + 1) * seq_length > images.shape[1]:
                 image_batch = images[:, batch * seq_length:, :, :, :]
             else:
@@ -43,7 +44,7 @@ def eval(model, split, seq_length, n_cpu, disp):
         if disp:
             print(i, c)
         correct.append(c)
-    PCE = np.mean(correct)
+    PCE = np.mean(correct) # Percentage of Correct Events
     return PCE
 
 
@@ -51,20 +52,22 @@ if __name__ == '__main__':
 
     split = 1
     seq_length = 64
-    n_cpu = 6
+    n_cpu = 1
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    model = EventDetector(pretrain=True,
+    model = Handy(pretrain=True,
                           width_mult=1.,
                           lstm_layers=1,
                           lstm_hidden=256,
+                          device = device,
                           bidirectional=True,
                           dropout=False)
 
-    save_dict = torch.load('models/swingnet_1800.pth.tar')
+    save_dict = torch.load('models/net_1800.pth.tar')
     model.load_state_dict(save_dict['model_state_dict'])
-    model.cuda()
+    model.to(device)
     model.eval()
-    PCE = eval(model, split, seq_length, n_cpu, True)
+    PCE = eval(model, split, seq_length, n_cpu, device, True)
     print('Average PCE: {}'.format(PCE))
 
 
