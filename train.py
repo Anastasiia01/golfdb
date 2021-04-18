e0776c2d33dc1dabd22ff6841ce2f0dd8116a2b4
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-04-17 23:41:00 -0400

    Changed ratio of events to no-events.

--- train.py
+++ train.py
@@ -35,23 +35,23 @@ if __name__ == '__main__':
                         vid_dir='data/handwash_videos_160/',
                         seq_length=seq_length,
                         transform=transforms.Compose([ToTensor(),
-                                                   Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+                                                   Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]), # ImageNet mean and std (RGB) correspondingly.
                         train=True)
-    print(dataset.__getitem__(20)) #checking that dataset is properly defined
+    # print(dataset.__getitem__(20)) #checking that dataset is properly defined
 
-    """data_loader = DataLoader(dataset,
+    data_loader = DataLoader(dataset,
                              batch_size=bs,
                              shuffle=True,
                              num_workers=n_cpu,
                              drop_last=True)
-
-    # the 8 golf swing events are classes 0 through 7, no-event is class 8
-    # the ratio of events to no-events is approximately 1:35 so weight classes accordingly:
-    weights = torch.FloatTensor([1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8, 1/35]).cuda()
+    
+    # the 4 handwashing events are classes 0 through 3, no-event is class 4
+    # the ratio of events to no-events is approximately 1:250 so weight classes accordingly:
+    weights = torch.FloatTensor([1/4, 1/4, 1/4, 1/4, 1/250]).to(device)
     criterion = torch.nn.CrossEntropyLoss(weight=weights)
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.001)
 
-    losses = AverageMeter()
+    losses = AverageMeter() 
 
     if not os.path.exists('models'):
         os.mkdir('models')
@@ -59,22 +59,21 @@ if __name__ == '__main__':
     i = 0
     while i < iterations:
         for sample in data_loader:
-            images, labels = sample['images'].cuda(), sample['labels'].cuda()
+            images, labels = sample['images'].to(device), sample['labels'].to(device)
             logits = model(images)
             labels = labels.view(bs*seq_length)
             loss = criterion(logits, labels)
             optimizer.zero_grad()
             loss.backward()
-            losses.update(loss.item(), images.size(0))
+            losses.update(loss.item(), images.size(0)) 
+            """loss.item() is the value of “total cost, or, sum of target*log(prediction)” 
+            averaged across all training examples of the current batch, according to the definition
+             of cross entropy loss."""
             optimizer.step()
             print('Iteration: {}\tLoss: {loss.val:.4f} ({loss.avg:.4f})'.format(i, loss=losses))
             i += 1
             if i % it_save == 0:
                 torch.save({'optimizer_state_dict': optimizer.state_dict(),
-                            'model_state_dict': model.state_dict()}, 'models/swingnet_{}.pth.tar'.format(i))
+                            'model_state_dict': model.state_dict()}, 'models/net_{}.pth.tar'.format(i))
             if i == iterations:
                 break
-    """
-
-
-
